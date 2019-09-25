ceaad45704c67a9cbbe98b0c499ba0529e647bed
dyrandy <jihun8862@gmail.com>
2019-09-25 15:05:24 +0900

    fixed html error and removed comments in controller

--- src/main/java/com/web/controller/BoardController.java
+++ src/main/java/com/web/controller/BoardController.java
@@ -17,13 +17,13 @@ public class BoardController {
 
     @Autowired
     BoardService boardService;
-//
-//    @GetMapping({"", "/"})
-//    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model){
-//        model.addAttribute("board", boardService.findBoardByIdx(idx));
-//        return "/board/form";
-//    }
-//
+
+    @GetMapping({"", "/"})
+    public String board(@RequestParam(value = "idx", defaultValue = "0") Long idx, Model model){
+        model.addAttribute("board", boardService.findBoardByIdx(idx));
+        return "/board/form";
+    }
+
     @GetMapping("/list")
     public String list(@PageableDefault Pageable pageable, Model model){
         model.addAttribute("boardList", boardService.findBoardList(pageable));
